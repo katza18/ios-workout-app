9d8fc150a3f3f3c9a5f9808dd5691b1f9a9c79e2
Aidan Katz <akatz42@gatech.edu>
2023-08-09 15:13:00 -0400

    Added workout logging

--- Workout/Views/WorkoutLogView.swift
+++ Workout/Views/WorkoutLogView.swift
@@ -13,14 +13,11 @@ struct WorkoutLogView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
     
     @State var workout: Workout?
-    @State var oldExercises: [Exercise]?
+    @State var oldExercises: [Exercise]
     @State private var loggedExercises: [Exercise] = []
     @State private var newExercises: [Exercise] = []
     
     var body: some View {
-        
-        var newWorkout = DataController().addWorkout(name: workout?.name ?? "", exercises: [], desc: workout?.desc ?? "", template: false, context: managedObjectContext)
-        
         VStack {
             Text("Timer: 0:00")
                 .font(.subheadline)
@@ -34,15 +31,31 @@ struct WorkoutLogView: View {
                 }
             }
             List {
-                
+                ForEach(loggedExercises.indices, id: \.self) { index in
+                    //Section for each new exercise
+                    Section {
+                        ForEach(loggedExercises[index].reps.indices, id: \.self) { repIndex in
+                            //For each set of the exercise, create a row
+                            HStack {
+                                Text("Weight")
+                                TextField("\(oldExercises[index].weight[repIndex]) lbs", value: $loggedExercises[index].weight[repIndex], format: .number)
+                                    .keyboardType(.numberPad)
+                                Text("x")
+                                TextField("\(oldExercises[index].reps[repIndex]) reps", value: $loggedExercises[index].reps[repIndex], format: .number)
+                                    .keyboardType(.numberPad)
+                            }
+                        }
+                    } header: {
+                        Text(loggedExercises[index].name)
+                    } //TODO: Add footer for plus/minus buttons
+                }
             }
             .listStyle(.plain)
             HStack {
                 Spacer()
                 Button("Done") {
-                    //Add exercises array to workout and save context
-//                    workout.exercises = loggedExercises
-//                    DataController().save(context: managedObjectContext)
+                    //Create the new workout log, this will also save changes to the new exercises
+                    let _ = DataController().addWorkout(name: workout?.name ?? "", exercises: loggedExercises, desc: workout?.desc ?? "", template: false, context: managedObjectContext)
                     
                     //Dismiss the view
                     dismiss()
@@ -58,21 +71,29 @@ struct WorkoutLogView: View {
         .navigationTitle("Workout Name")
         .navigationBarBackButtonHidden()
         .onAppear {
-//            ForEach(oldExercises ?? []) { exercise in
-//                let sets = Int(exercise.sets)
-//
-//                ForEach(0..<(sets ?? 1), id: \.self) { _ in
-//                    let newExercise = DataController().addExercise(name: exercise.name, sets: "1", reps: exercise.reps, rest: exercise.rest, template: false, weight: exercise.weight, context: managedObjectContext)
-//                    let _ = newExercises.append(newExercise)
-//                }
-//            }
+            //Create the logged exercises when the view renders
+            loggedExercises = createLoggedExercises()
         }
+    }
+    
+    private func createLoggedExercises() -> [Exercise] {
+        //Create a new array of exercises
+        var newExercises : [Exercise] = []
+        
+        for oldExercise in oldExercises {
+            //Create newExercise from template and add to array
+            let newExercise = DataController().addExercise(name: oldExercise.name, reps: oldExercise.reps, rest: oldExercise.rest, template: false, weight: oldExercise.weight, context: managedObjectContext)
+            newExercises.append(newExercise)
+        }
+        
+        //Return the new array
+        return newExercises
     }
 }
 
 struct WorkoutLogView_Previews: PreviewProvider {
     static var previews: some View {
-        WorkoutLogView()
+        WorkoutLogView(oldExercises: [])
     }
 }
 
